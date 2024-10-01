9188b2f3fae27cf49f68baee950265344bda9744
tsmith422 <smith.taylor@tamu.edu>
2024-10-01 13:36:16 -0500

    Completed the init function, initialized contexts and current_context_idx

--- Lab3/threading.c
+++ Lab3/threading.c
@@ -3,6 +3,15 @@
 void t_init()
 {
         // TODO
+        // initialize contexts
+        for (int i = 0; i < NUM_CTX; ++i)
+        {
+                contexts[i].state = INVALID;
+                memset(&(contexts[i].context), 0, sizeof(ucontext_t));
+        }
+
+        // initialize current_context_idx
+        current_context_idx = 0;
 }
 
 int32_t t_create(fptr foo, int32_t arg1, int32_t arg2)
