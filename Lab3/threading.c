0c7412d26452838c9475decc1b1a75c94041d797
tsmith422 <smith.taylor@tamu.edu>
2024-10-03 13:45:12 -0500

    finished the t_create function and switched contexts in t_yield

--- Lab3/threading.c
+++ Lab3/threading.c
@@ -17,11 +17,46 @@ void t_init()
 int32_t t_create(fptr foo, int32_t arg1, int32_t arg2)
 {
         // TODO
+        for (volatile int empty_idx = 0; empty_idx < NUM_CTX; ++empty_idx)
+        {
+                // Find empty entry (INVALID state)
+                if (contexts[empty_idx].state == INVALID)
+                {
+                        getcontext(&contexts[empty_idx].context);
+
+                        // Allocate stack??
+                        contexts[empty_idx].context.uc_stack.ss_sp = (char *)malloc(STK_SZ);
+                        contexts[empty_idx].context.uc_stack.ss_size = STK_SZ;
+                        contexts[empty_idx].context.uc_stack.ss_flags = 0;
+                        contexts[empty_idx].context.uc_link = NULL;
+
+                        // Modify content
+                        makecontext(&contexts[empty_idx].context, (void (*)())foo, 2, arg1, arg2);
+
+                        // Set state so it's ready to be used (VALID)
+                        contexts[empty_idx].state = VALID;
+                        return 0;
+                }
+        }
+
+        return 1;
 }
 
 int32_t t_yield()
 {
         // TODO
+        getcontext(&contexts[current_context_idx].context);
+        for (int i = 0; i < NUM_CTX; ++i)
+        {
+                if (contexts[i].state == VALID)
+                {
+                        int8_t old_current = current_context_idx;
+                        current_context_idx = (uint8_t)i;
+                        swapcontext(&contexts[old_current].context, &contexts[current_context_idx].context);
+                }
+        }
+
+        return 0;
 }
 
 void t_finish()
